2a18d7c596098dfb7a8a8dc61ff4c05b0fdafd6b
Santy295 <santynavarro29@hotmail.com>
2020-08-02 18:06:55 -0500

    Cambio 6 dia 2

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 // const path = require('path');
 const app = express();
-app.use(express.static('tp1'));
+app.use(express.static('./dist/tp1'));
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'tp1' }
+    res.sendFile('index.html', { root: 'dist/tp1' }
     );
   });
 // app.get('/*', function(req,res) {
