49b926db0debb135010750fc8365248fbbfa177c
Santy295 <santynavarro29@hotmail.com>
2020-08-01 19:58:39 -0500

    Error en postinstall

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/tp1/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
