2655538ee5fe111fe70bfc856b9fe337deb6c526
Santy295 <santynavarro29@hotmail.com>
2020-08-02 18:50:57 -0500

    Cambio 9 dia 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const path = require('path');
 const app = express();
-// app.use(express.static('./dist/tp1'));
+app.use(express.static('./dist/tp1'));
 app.get('/*', function (req, res) {
     res.sendFile('index.html', { root: 'dist/tp1' }
     );
